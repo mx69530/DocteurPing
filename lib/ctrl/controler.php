573a8653929bcd01904dcd2714ee5d4398af7726
root <root@Falco-Laptop>
2015-02-26 13:51:08 +0100

    Ajout du controlleur, formalisation du code

---
+++ lib/ctrl/controler.php
@@ -0,0 +1,21 @@
+<?php
+	class Controler
+	{
+		private $_variable;
+
+		/**
+			Contructeur
+		*/
+		public function __construct() {
+			
+		}
+
+		/**
+			Affichage de la vue dans index.php
+		*/
+		public function currentView(){
+
+		}
+	}
+
+?>
